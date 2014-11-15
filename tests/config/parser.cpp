0f52f0e2bbf7561bd3a9d1ed701eb3f14fbbd23c
Andrey Goryachev <andrey.goryachev@gmail.com>
2014-11-16 01:23:46 +0300

    And now config_parser_t is tested.

--- tests/config/parser.cpp
+++ tests/config/parser.cpp
@@ -23,137 +23,222 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #include "kora/config/parser.hpp"
 
+#include <fstream>
+
 TEST(ConfigParser, DefaultConstructor) {
     kora::config_parser_t parser;
     EXPECT_TRUE(parser.root().underlying_object().is_null());
 }
 
-TEST(ConfigParser, NumericTest) {
-    std::string json =
-            "{\n"
-            "    \"short_min\": -32768,\n"
-            "    \"short_max\": 32767,\n"
-            "    \"ushort_min\": 0,\n"
-            "    \"ushort_max\": 65535,\n"
-            "    \"int64_min\": -9223372036854775808,\n"
-            "    \"int64_max\": 9223372036854775807,\n"
-            "    \"uint64_min\": 0,\n"
-            "    \"uint64_max\": 18446744073709551615\n"
-            "}\n"
-    ;
+namespace {
 
-    std::istringstream stream(json);
+    void
+    test_numbers_parsing(std::function<void(std::string, kora::config_parser_t&)> parse) {
+        std::string json =
+                "{\n"
+                "    \"short_min\": -32768,\n"
+                "    \"short_max\": 32767,\n"
+                "    \"ushort_min\": 0,\n"
+                "    \"ushort_max\": 65535,\n"
+                "    \"int64_min\": -9223372036854775808,\n"
+                "    \"int64_max\": 9223372036854775807,\n"
+                "    \"uint64_min\": 0,\n"
+                "    \"uint64_max\": 18446744073709551615\n"
+                "}\n"
+        ;
 
-    kora::config_parser_t parser;
-    parser.parse(stream);
+        kora::config_parser_t parser;
+        parse(json, parser);
 
-    kora::config_t config = parser.root();
+        kora::config_t config = parser.root();
 
-    GTEST_ASSERT_EQ(std::numeric_limits<short>::min(), config.at<short>("short_min"));
-    GTEST_ASSERT_EQ(std::numeric_limits<short>::max(), config.at<short>("short_max"));
-    GTEST_ASSERT_EQ(std::numeric_limits<unsigned short>::min(), config.at<unsigned short>("ushort_min"));
-    GTEST_ASSERT_EQ(std::numeric_limits<unsigned short>::max(), config.at<unsigned short>("ushort_max"));
-    GTEST_ASSERT_EQ(std::numeric_limits<int64_t>::min(), config.at<int64_t>("int64_min"));
-    GTEST_ASSERT_EQ(std::numeric_limits<int64_t>::max(), config.at<int64_t>("int64_max"));
-    GTEST_ASSERT_EQ(std::numeric_limits<uint64_t>::min(), config.at<uint64_t>("uint64_min"));
-    GTEST_ASSERT_EQ(std::numeric_limits<uint64_t>::max(), config.at<uint64_t>("uint64_max"));
+        ASSERT_EQ(std::numeric_limits<short>::min(), config.at<short>("short_min"));
+        ASSERT_EQ(std::numeric_limits<short>::max(), config.at<short>("short_max"));
+        ASSERT_EQ(std::numeric_limits<unsigned short>::min(), config.at<unsigned short>("ushort_min"));
+        ASSERT_EQ(std::numeric_limits<unsigned short>::max(), config.at<unsigned short>("ushort_max"));
+        ASSERT_EQ(std::numeric_limits<int64_t>::min(), config.at<int64_t>("int64_min"));
+        ASSERT_EQ(std::numeric_limits<int64_t>::max(), config.at<int64_t>("int64_max"));
+        ASSERT_EQ(std::numeric_limits<uint64_t>::min(), config.at<uint64_t>("uint64_min"));
+        ASSERT_EQ(std::numeric_limits<uint64_t>::max(), config.at<uint64_t>("uint64_max"));
+    }
+
+    void
+    test_basic_parsing(std::function<void(std::string, kora::config_parser_t&)> parse) {
+        std::string json =
+                "{\n"
+                "    \"int_key\": 1337,\n"
+                "    \"string_key\": \"@_@\",\n"
+                "    \"array_key\": [1, true, \"%)))\"],\n"
+                "    \"object_key\": {\n"
+                "        \"key\": \"value\"\n"
+                "    }\n"
+                "}\n"
+        ;
+
+        kora::config_parser_t parser;
+        parse(json, parser);
+
+        kora::config_t config = parser.root();
+
+        ASSERT_EQ(1337, config.at<int>("int_key"));
+        ASSERT_EQ("@_@", config.at<std::string>("string_key"));
+        ASSERT_EQ(1, config.at("array_key").at<int>(0));
+        ASSERT_EQ(true, config.at("array_key").at<bool>(1));
+        ASSERT_EQ("%)))", config.at("array_key").at<std::string>(2));
+        ASSERT_EQ("value", config.at("object_key").at<std::string>("key"));
+    }
+
+    void
+    test_parsing_error_1(std::function<void(std::string, kora::config_parser_t&)> parse) {
+        std::string json =
+                "{\n"
+                "    \"short_min\": -32768,\n"
+                "    \"short_max\": 32767,\n"
+                "    \"ushort_min\": 0,\n"
+                "    \"ushort_max\": 65535,\n"
+                "    \"int64_min\": -9223372036854775808,\n"
+                "    \"int64_max\": 9223372036854775807,\n"
+                "    \"uint64_min\": 0,\n"
+                "    \"uint64_max\": 18446744073709551615-\n"
+                "}\n"
+        ;
+
+        kora::config_parser_t parser;
+
+        try {
+            parse(json, parser);
+            GTEST_FAIL();
+        } catch (kora::config_parser_error_t &error) {
+            GTEST_ASSERT_EQ(9, error.line_number());
+            GTEST_ASSERT_EQ(39, error.column_number());
+        }
+    }
+
+    void
+    test_parsing_error_2(std::function<void(std::string, kora::config_parser_t&)> parse) {
+        std::string json = "s{\n";
+
+        kora::config_parser_t parser;
+
+        try {
+            parse(json, parser);
+            GTEST_FAIL();
+        } catch (kora::config_parser_error_t &error) {
+            GTEST_ASSERT_EQ(1, error.line_number());
+            GTEST_ASSERT_EQ(1, error.column_number());
+        }
+    }
+
+    void
+    test_parsing_error_3(std::function<void(std::string, kora::config_parser_t&)> parse) {
+        std::string json = "{s\n";
+
+        kora::config_parser_t parser;
+
+        try {
+            parse(json, parser);
+            GTEST_FAIL();
+        } catch (kora::config_parser_error_t &error) {
+            GTEST_ASSERT_EQ(1, error.line_number());
+            GTEST_ASSERT_EQ(2, error.column_number());
+        }
+    }
+
+    void
+    test_that_anything_after_config_isnt_allowed(std::function<void(std::string, kora::config_parser_t&)> parse) {
+        std::string json =
+                "{\n"
+                "    \"key1\": -1,\n"
+                "    \"key2\": true\n"
+                "}garbage\n"
+        ;
+
+        kora::config_parser_t parser;
+
+        try {
+            parse(json, parser);
+            GTEST_FAIL();
+        } catch (kora::config_parser_error_t &error) {
+            ASSERT_EQ(4, error.line_number());
+            ASSERT_EQ(2, error.column_number());
+        }
+    }
+
+    void
+    test_that_config_must_be_an_object(std::function<void(std::string, kora::config_parser_t&)> parse) {
+        std::string json =
+                "  [\n"
+                "      -1,\n"
+                "      true\n"
+                "  ]\n"
+        ;
+
+        kora::config_parser_t parser;
+
+        try {
+            parse(json, parser);
+            GTEST_FAIL();
+        } catch (kora::config_parser_error_t &error) {
+            ASSERT_EQ(1, error.line_number());
+            ASSERT_EQ(3, error.column_number());
+        }
+    }
+
+    void
+    test_parser(std::function<void(std::string, kora::config_parser_t&)> parse) {
+        test_basic_parsing(parse);
+        test_numbers_parsing(parse);
+        test_parsing_error_1(parse);
+        test_parsing_error_2(parse);
+        test_parsing_error_3(parse);
+        test_that_anything_after_config_isnt_allowed(parse);
+        test_that_config_must_be_an_object(parse);
+    }
+
+    const std::string tmpfilename = "config_parser_test_tmp_file_dont_touch_this_ijhiu47837y8hc";
+
+    void
+    method_parse_wrapper(std::string json, kora::config_parser_t &parser) {
+        std::istringstream stream(json);
+        parser.parse(stream);
+    }
+
+    void
+    method_open_wrapper(std::string json, kora::config_parser_t &parser) {
+        std::ofstream tmpfile(tmpfilename);
+        tmpfile.write(json.data(), json.size());
+        tmpfile.close();
+        parser.open(tmpfilename);
+    }
+
+    void
+    stream_constructor_wrapper(std::string json, kora::config_parser_t &parser) {
+        std::istringstream stream(json);
+        parser = kora::config_parser_t(stream);
+    }
+
+    void
+    file_constructor_wrapper(std::string json, kora::config_parser_t &parser) {
+        std::ofstream tmpfile(tmpfilename);
+        tmpfile.write(json.data(), json.size());
+        tmpfile.close();
+        parser = kora::config_parser_t(tmpfilename);
+    }
+
+} // namespace
+
+TEST(ConfigParser, StreamConstructor) {
+    test_parser(&stream_constructor_wrapper);
 }
 
-
-TEST(ConfigParser, ParsingError1) {
-    std::string json =
-            "{\n"
-            "    \"short_min\": -32768,\n"
-            "    \"short_max\": 32767,\n"
-            "    \"ushort_min\": 0,\n"
-            "    \"ushort_max\": 65535,\n"
-            "    \"int64_min\": -9223372036854775808,\n"
-            "    \"int64_max\": 9223372036854775807,\n"
-            "    \"uint64_min\": 0,\n"
-            "    \"uint64_max\": 18446744073709551615-\n"
-            "}\n"
-    ;
-
-    std::istringstream stream(json);
-
-    kora::config_parser_t parser;
-    try {
-        parser.parse(stream);
-        GTEST_FAIL();
-    } catch (kora::config_parser_error_t &error) {
-        GTEST_ASSERT_EQ(9, error.line_number());
-        GTEST_ASSERT_EQ(39, error.column_number());
-    }
+TEST(ConfigParser, FileConstructor) {
+    test_parser(&file_constructor_wrapper);
 }
 
-TEST(ConfigParser, ParsingError2) {
-    std::string json = "s{\n";
-
-    std::istringstream stream(json);
-
-    kora::config_parser_t parser;
-
-    try {
-        parser.parse(stream);
-        GTEST_FAIL();
-    } catch (kora::config_parser_error_t &error) {
-        GTEST_ASSERT_EQ(1, error.line_number());
-        GTEST_ASSERT_EQ(1, error.column_number());
-    }
+TEST(ConfigParser, Parse) {
+    test_parser(&method_parse_wrapper);
 }
 
-TEST(ConfigParser, ParsingError3) {
-    std::string json = "{s\n";
-
-    std::istringstream stream(json);
-
-    kora::config_parser_t parser;
-
-    try {
-        parser.parse(stream);
-        GTEST_FAIL();
-    } catch (kora::config_parser_error_t &error) {
-        GTEST_ASSERT_EQ(1, error.line_number());
-        GTEST_ASSERT_EQ(2, error.column_number());
-    }
-}
-
-TEST(ConfigParser, AnythingAfterConfigIsNotAllowed) {
-    std::string json =
-            "{\n"
-            "    \"key1\": -1,\n"
-            "    \"key2\": true\n"
-            "}garbage\n"
-    ;
-
-    std::istringstream stream(json);
-
-    kora::config_parser_t parser;
-    try {
-        parser.parse(stream);
-        FAIL();
-    } catch (kora::config_parser_error_t &error) {
-        ASSERT_EQ(4, error.line_number());
-        ASSERT_EQ(2, error.column_number());
-    }
-}
-
-TEST(ConfigParser, ConfigMustBeAnObject) {
-    std::string json =
-            "  [\n"
-            "      -1,\n"
-            "      true\n"
-            "  ]\n"
-    ;
-
-    std::istringstream stream(json);
-
-    kora::config_parser_t parser;
-    try {
-        parser.parse(stream);
-        FAIL();
-    } catch (kora::config_parser_error_t &error) {
-        ASSERT_EQ(1, error.line_number());
-        ASSERT_EQ(3, error.column_number());
-    }
+TEST(ConfigParser, Open) {
+    test_parser(&method_open_wrapper);
 }
